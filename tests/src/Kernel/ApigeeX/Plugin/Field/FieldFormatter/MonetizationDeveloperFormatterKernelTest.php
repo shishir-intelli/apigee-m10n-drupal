3635d40666def40c41cd301c3680c8e41dc34463
Shishir <75600200+shishir-intelli@users.noreply.github.com>
2025-11-11 17:47:09 +0530

    new test 1

--- tests/src/Kernel/ApigeeX/Plugin/Field/FieldFormatter/MonetizationDeveloperFormatterKernelTest.php
+++ tests/src/Kernel/ApigeeX/Plugin/Field/FieldFormatter/MonetizationDeveloperFormatterKernelTest.php
@@ -19,11 +19,14 @@
 
 namespace Drupal\Tests\apigee_m10n\Kernel\ApigeeX\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\DependencyInjection\ContainerBuilder; // ADD THIS
 use Drupal\Core\Field\FieldItemList;
 use Drupal\Tests\apigee_m10n\Kernel\ApigeeX\MonetizationKernelTestBase;
 use Drupal\apigee_m10n\MonetizationInterface;
 use Drupal\apigee_m10n\Plugin\Field\FieldFormatter\MonetizationDeveloperFormatter;
 use Drupal\apigee_m10n\Plugin\Field\FieldType\MonetizationDeveloperFieldItem;
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\Core\Site\Settings;
 
 /**
  * Test the `apigee_monetization_developer` field formatter.
@@ -77,26 +80,65 @@ class MonetizationDeveloperFormatterKernelTest extends MonetizationKernelTestBas
 
   /**
    * {@inheritdoc}
+   */
+  public function register(ContainerBuilder $container) {
+    parent::register($container);
+    // Ensure the private stream wrapper service is registered in the container
+    // for this test. This is sometimes necessary in kernel tests if the
+    // full module set isn't loaded.
+    if (!$container->hasDefinition('stream_wrapper.private')) {
+      $container->register('stream_wrapper.private', 'Drupal\Core\StreamWrapper\PrivateStream')
+        ->addTag('stream_wrapper', ['scheme' => 'private']);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
    *
    * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \Exception
    */
   protected function setUp(): void {
     parent::setUp();
 
+    // At this point, parent::setUp() should have run, and the register() method above
+    // should have been called, ensuring the stream_wrapper.private service definition exists.
+    // The apigee_edge base classes likely set 'file_private_path' to 'vfs://root/private'.
+
+    if (!in_array('private', stream_get_wrappers())) {
+        $settings = Settings::getInstance();
+        $actual_private_path = $settings ? $settings->get('file_private_path') : 'Settings service not available';
+        throw new \RuntimeException(
+            "The 'private' stream wrapper is NOT registered after parent::setUp() and register(). " .
+            "Actual 'file_private_path' setting: " . var_export($actual_private_path, TRUE)
+        );
+    }
+
+    // Get the file system service.
+    $file_system = $this->container->get('file_system');
+    $options = FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS;
+
+    // Prepare the directories within the file system (likely VFS).
+    $oauth_uri = 'private://apigee/oauth';
+    if (!$file_system->prepareDirectory($oauth_uri, $options)) {
+        throw new \RuntimeException("Failed to create directory at " . $oauth_uri);
+    }
+
+    // Install schemas and configs.
     $this->installEntitySchema('user');
     $this->installSchema('user', ['users_data']);
     $this->installConfig([
       'user',
     ]);
 
-    // Do not use user 1.
-    $this->createAccount();
-
+    // Create test accounts.
+    $this->createAccount(); // User 1
     $this->developer = $this->createAccount(MonetizationInterface::DEFAULT_AUTHENTICATED_PERMISSIONS);
 
     // Get pre-configured token storage service for testing.
     $this->storeToken();
 
+    // Create test entities.
     $this->stack->reset();
     $this->xproduct = $this->createApigeexProduct();
     $this->stack->reset();
@@ -107,6 +149,7 @@ class MonetizationDeveloperFormatterKernelTest extends MonetizationKernelTestBas
     $this->purchasedProduct = $this->createPurchasedProduct($this->developer, $this->ratePlan);
     $this->stack->reset();
 
+    // Get services from the container.
     $this->formatterManager = $this->container->get('plugin.manager.field.formatter');
     $this->fieldManager = $this->container->get('entity_field.manager');
   }
@@ -141,7 +184,7 @@ class MonetizationDeveloperFormatterKernelTest extends MonetizationKernelTestBas
     static::assertSame($this->purchasedProduct->getDeveloper()->getName(), (string) $build[0]['#markup']);
 
     $this->render($build);
-    $this->assertText($this->purchasedProduct->getDeveloper()->getName());
+    $this->assertSession()->pageTextContains($this->purchasedProduct->getDeveloper()->getName());
   }
 
 }
